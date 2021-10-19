3e5e50ff231c6d79e0908d6ea867f94e707b53e2
Douglas Oliveira <2doug077@gmail.com>
2021-10-18 22:56:04 -0300

    land page principal

--- index.php
+++ index.php
@@ -1,58 +1,219 @@
 <ht<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <html lang="en">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
-    <title>InTi</title>
-  </head>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
 
 
-  <body>
-      <section>
-        <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap" rel="stylesheet">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
+        <title>InTi</title>
+    </head>
+
+    <body>
+        <!-- HEADER -->
+        <div id="header">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light ">
+                    <a class="navbar-brand" href="#">
+                        <img src="img/logoP.png" class="image-fluid">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Página inicial</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Eventos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Sobre</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- //HEADER -->
+
+        <!-- Carousel -->
+        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-interval="10000">
+                    <img src="./img/ev1.png" class="d-block w-100" alt="...">
                 </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-            </form>
+                <div class="carousel-item" data-interval="2000">
+                    <img src="./img/ev2.jpeg" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
         </div>
-    </nav>
-        </div>
-      </section>
+        <!-- //Carousel -->
 
-  </body>
-</html>
+
+        <!-- SLIDER -->
+        <div id="slider">
+            <div class="container pt-5">
+                <div class="row mb-5">
+                    <div class="col-lg-4 align-self-center ">
+                        <h1>Hoje é seu dia</h1>
+                        <h4 class="mb-4">Faça ja a reserva do seu evento preferido</h4>
+                        <a href="" class="btn button-primary btn-dark button d-md-inline-block d-block mb-md-0 mb-2">Eventos</a>
+                        <a href="" class="btn button-outline-primary  btn-outline-dark button d-md-inline-block d-block mb-md-0 mb-2">Contatos</a>
+                    </div>
+                    <div class="col-lg-8 align-self-center text-center">
+                        <img src="./img/top.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //SLIDER -->
+        <div class="container mb-5">
+            <h1>Em Alta</h1>
+            <div class="row d-flex justify-content-around">
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+            </div>
+            <div class="container d-flex justify-content-end">
+                <a href="" class="btn button-outline-primary  btn-outline-dark button d-md-inline-block d-block mb-md-0 mt-5 ">Ver todos eventos</a>
+            </div>
+
+        </div>
+
+        <div class="container mb-5">
+            <h1>Ultimos eventos</h1>
+            <div class="row d-flex justify-content-around">
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+
+                <div class="card m-2">
+                    <img src="./img/card1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Evento</h5>
+                        <p class="card-text">Evento exclusivo, ingressos para pista e camarote, ultimas unidades</p>
+                        <a href="#" class="btn btn button-primary btn-dark button">Comprar ingresso</a>
+                    </div>
+                </div>
+            </div>
+            <div class="container d-flex justify-content-end">
+                <a href="" class="btn button-outline-primary  btn-outline-dark button d-md-inline-block d-block mb-md-0 mt-5 ">Ver todos eventos</a>
+            </div>
+        </div>
+
+        <!-- Footer-->
+
+        <footer class="bg-light text-center text-white">
+            <!-- Grid container -->
+            <div class="container p-4 pb-0">
+                <!-- Section: Social media -->
+                <section class="mb-4">
+                    <!-- Facebook -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #3b5998; border: transparent" href="#!" role="button"><i class="bi-facebook"></i></a>
+
+                    <!-- Twitter -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #55acee; border: transparent" href="#!" role="button"><i class="bi bi-twitter"></i></a>
+
+                    <!-- Google -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #dd4b39; border: transparent" href="#!" role="button"><i class="bi bi-youtube"></i></a>
+
+                    <!-- Instagram -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #ac2bac; border: transparent" href="#!" role="button"><i class="bi bi-instagram"></i></a>
+
+                    <!-- Linkedin -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #0082ca; border: transparent" href="#!" role="button"><i class="bi bi-linkedin"></i></a>
+                    <!-- Github -->
+                    <a class="btn btn-dark btn-floating m-1" style="background-color: #333333; border: transparent" href="#!" role="button"><i class="bi bi-github"></i></a>
+                </section>
+                <!-- Section: Social media -->
+            </div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2020 
+                <a class="text-white" href="#">Copyright</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <script src="js/bootstrap.js"></script>
+    </body>
+
+    </html>
--- login.php
+++ login.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-    <link rel="stylesheet" href="styleLogin.css">
+    <link rel="stylesheet" href="./css/styleLogin.css">
+    
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
